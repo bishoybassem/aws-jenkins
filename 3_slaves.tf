9e5575bc053885be3374fe598441b9a4c2aaa498
bishoybassem <bishoybassem@gmail.com>
2019-03-08 17:19:57 +0100

    Scale up the slaves in proportion to the build queue

--- 3_slaves.tf
+++ 3_slaves.tf
@@ -38,9 +38,8 @@ resource "aws_launch_template" "jenkins_slave_launch_template" {
   }
 }
 
-resource "aws_autoscaling_group" "jenkins_slave_autoscaling_group" {
+resource "aws_autoscaling_group" "jenkins_slaves_autoscaling_group" {
   name                = "jenkins_slaves"
-  desired_capacity    = "${var.slave_count}"
   min_size            = "${var.slave_count}"
   max_size            = "${var.slave_max_count}"
   vpc_zone_identifier = ["${aws_subnet.main_private.id}"]
@@ -49,5 +48,36 @@ resource "aws_autoscaling_group" "jenkins_slave_autoscaling_group" {
   launch_template {
     id      = "${aws_launch_template.jenkins_slave_launch_template.id}"
     version = "$$Latest"
+  }
+}
+
+resource "aws_autoscaling_policy" "jenkins_slaves_scale_up_policy" {
+  name                      = "jenkins_slaves_scale_up_policy"
+  policy_type               = "StepScaling"
+  adjustment_type           = "ChangeInCapacity"
+  autoscaling_group_name    = "${aws_autoscaling_group.jenkins_slaves_autoscaling_group.name}"
+  estimated_instance_warmup = 120
+
+  step_adjustment {
+    scaling_adjustment          = 1
+    metric_interval_lower_bound = 0
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "jenkins_long_waiting_queue" {
+  alarm_name          = "jenkins_long_waiting_queue"
+  alarm_description   = "Trigger scaling up policy if queue has more than 2 builds waiting for at least 5 minutes"
+  namespace           = "CWAgent"
+  metric_name         = "jenkins_queue"
+  period              = 60
+  statistic           = "Average"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = 2
+  evaluation_periods  = 5
+  alarm_actions       = ["${aws_autoscaling_policy.jenkins_slaves_scale_up_policy.arn}"]
+
+  dimensions {
+    InstanceId  = "${aws_instance.jenkins_master.id}"
+    metric_type = "gauge"
   }
 }
