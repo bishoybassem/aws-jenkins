10acd1754c9ef3cc226726454fefbf0374990850
bishoybassem <bishoybassem@gmail.com>
2019-03-13 09:01:24 +0100

    Scale down the slaves in case the build queue is empty

--- 3_slaves.tf
+++ 3_slaves.tf
@@ -56,7 +56,7 @@ resource "aws_autoscaling_policy" "jenkins_slaves_scale_up_policy" {
   policy_type               = "StepScaling"
   adjustment_type           = "ChangeInCapacity"
   autoscaling_group_name    = "${aws_autoscaling_group.jenkins_slaves_autoscaling_group.name}"
-  estimated_instance_warmup = 120
+  estimated_instance_warmup = 180
 
   step_adjustment {
     scaling_adjustment          = 1
@@ -66,7 +66,7 @@ resource "aws_autoscaling_policy" "jenkins_slaves_scale_up_policy" {
 
 resource "aws_cloudwatch_metric_alarm" "jenkins_long_waiting_queue" {
   alarm_name          = "jenkins_long_waiting_queue"
-  alarm_description   = "Trigger scaling up policy if queue has more than 2 builds waiting for at least 5 minutes"
+  alarm_description   = "Trigger scaling up policy if the queue has more than 2 builds waiting for at least 5 minutes"
   namespace           = "CWAgent"
   metric_name         = "jenkins_queue"
   period              = 60
@@ -75,6 +75,33 @@ resource "aws_cloudwatch_metric_alarm" "jenkins_long_waiting_queue" {
   threshold           = 2
   evaluation_periods  = 5
   alarm_actions       = ["${aws_autoscaling_policy.jenkins_slaves_scale_up_policy.arn}"]
+
+  dimensions {
+    InstanceId  = "${aws_instance.jenkins_master.id}"
+    metric_type = "gauge"
+  }
+}
+
+resource "aws_autoscaling_policy" "jenkins_slaves_scale_down_policy" {
+  name                      = "jenkins_slaves_scale_down_policy"
+  policy_type               = "SimpleScaling"
+  adjustment_type           = "ChangeInCapacity"
+  scaling_adjustment        = -1
+  cooldown                  = 120
+  autoscaling_group_name    = "${aws_autoscaling_group.jenkins_slaves_autoscaling_group.name}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "jenkins_empty_queue" {
+  alarm_name          = "jenkins_empty_queue"
+  alarm_description   = "Trigger scaling down policy if the queue is empty for at least 10 minutes"
+  namespace           = "CWAgent"
+  metric_name         = "jenkins_queue"
+  period              = 60
+  statistic           = "Average"
+  comparison_operator = "LessThanThreshold"
+  threshold           = 1
+  evaluation_periods  = 10
+  alarm_actions       = ["${aws_autoscaling_policy.jenkins_slaves_scale_down_policy.arn}"]
 
   dimensions {
     InstanceId  = "${aws_instance.jenkins_master.id}"
